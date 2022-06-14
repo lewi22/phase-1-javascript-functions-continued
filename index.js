0a7e29924d04e575c061a08358d53209fb8cd9fe
lewinsky <lewinskygatwirim@gmail.com>
2022-06-14 04:35:47 -0400

    commit tree 343c4af4eef872915792f48afcbda972715b279e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const saturdayFun = function(fun = 'roller-skate' , duty = 'bathe my dog') {
+  if (fun) {
+      return (`This Saturday, I want to ${fun}!`);
+  }
+  else if (duty)
+  return (`This Saturday, I want to ${duty}!`);
+}
+const mondayWork = function(work = 'go to the office' , home = 'work from home') {if (work) {
+      return (`This Monday, I will ${work}.`);
+  }
+  else if (home)
+  return (`This Monday, I will ${home}.`);
+}
+function wrapAdjective(wrap = '*' , special) {
+  if ('*') {
+      return (wrap => `You are a hard worker!`);
+  }
+  else if (`special`)
+  return (wrap =>  `You are ||a dedicated programmer||!`)
+}
